d40cd8346f3caefc6af90cc5884248500ee13879
quocvietht03 <quocvietht03@gmail.com>
2023-09-30 11:18:00 +0700

    update anchor hook block

--- block-parts/anchor-hook.php
+++ block-parts/anchor-hook.php
@@ -5,5 +5,5 @@
 
 $anchor_id = get_field('anchor_id');
 if(!empty($anchor_id)) {
-  echo '<div id="' . $anchor_id . '" class="pj-anchor-hook">Enter link #anchor_id to scroll here.</div>';
+  echo '<div id="' . $anchor_id . '" class="pj-anchor-hook">Enter link #'. $anchor_id .' to scroll here.</div>';
 }
